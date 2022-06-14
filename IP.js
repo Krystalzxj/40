a27a33b335227f6ab4443d582b21c24c4e446161
Krystalzxj <103553035+Krystalzxj@users.noreply.github.com>
2022-06-14 12:53:04 +0800

    Create IP.js

---
+++ IP.js
@@ -0,0 +1,25 @@
+let url = "http://ip-api.com/json"
+
+$httpClient.get(url, function(error, response, data){
+    let jsonData = JSON.parse(data)
+    let country = jsonData.country
+    let emoji = getFlagEmoji(jsonData.countryCode)
+    let city = jsonData.city
+    let isp = jsonData.isp
+    let ip = jsonData.query
+  body = {
+    title: "𝑵𝒐𝒅𝒆 𝒊𝒏𝒇𝒐𝒓𝒎𝒂𝒕𝒊𝒐𝒏 🛰",
+    content: `[𝑰𝑷 信息]: ${ip}\n[运营商]: ${isp}\n[所在地]: ${emoji} | ${country} - ${city}`,
+    icon: "globe.asia.australia.fill"
+  }
+  $done(body);
+});
+
+
+function getFlagEmoji(countryCode) {
+    const codePoints = countryCode
+      .toUpperCase()
+      .split('')
+      .map(char =>  127397 + char.charCodeAt());
+    return String.fromCodePoint(...codePoints);
+}
